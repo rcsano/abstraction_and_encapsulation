69d4706030386728da53ddf71ef165808d0905bd
unknown <rc.sanoiz@gmail.com>
2026-06-03 04:46:11 +0800

    implemented class constructor with private variables first huhu

--- fan.py
+++ fan.py
@@ -1,8 +1,16 @@
 
 # define constant speeds
+class Fan:
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
 
 # create private attributes for speed, radius, color, and status
-
+    def __init__(self, speed=SLOW, radius=5.0, color="blue", on=False):
+        self.speed = speed
+        self.radius = radius
+        self.color = color
+        self.on = on
+        
 # create getter methods to read values
-
 # create setter methods to change values
