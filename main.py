ddfd66ebe61bbd44c29ce076733beac3814a96ac
unknown <rc.sanoiz@gmail.com>
2026-06-03 14:22:25 +0800

    outline created

---
+++ main.py
@@ -0,0 +1,9 @@
+# import car class from module file
+
+# instantiate car object
+
+# sequence: acceleration (5 iterations)
+
+# sequence: deceleration (5 iterations)
+
+# display of real-time speed metrics
