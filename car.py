b76c6d89eec40bfea913af0f3a75d73489be8291
unknown <rc.sanoiz@gmail.com>
2026-06-03 14:14:37 +0800

    created the operational blueprint of the class, did not forget the name mangling na hehehe

--- car.py
+++ car.py
@@ -1,9 +1,21 @@
-# get car details (year_model, make)
+# get car details (make, year_model)
+class Car:
+    def __init__(self, make, model, year):
+        self.__make = make
+        self.__year_model = model
+        self.__speed = 0
 
 # create attributes with initial speed as 0
+    def get_speed(self):
+        return self.__speed
 
 # perform acceleration (add 5 to speed)
+    def accelerate(self):
+        self.__speed += 5
 
 # perform braking (subtract 5 from speed)
-
-# print speed results
+    def decelerate(self):
+        if self.__speed > 0:
+            self.__speed -= 5
+        else:
+            self.__speed = 0
