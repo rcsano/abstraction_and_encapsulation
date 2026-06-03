b15fe66d2910af0ce34515e001dfc596c37f53a6
unknown <rc.sanoiz@gmail.com>
2026-06-03 14:05:30 +0800

    created comment outline to follow

---
+++ car.py
@@ -0,0 +1,9 @@
+# get car details (year_model, make)
+
+# create attributes with initial speed as 0
+
+# perform acceleration (add 5 to speed)
+
+# perform braking (subtract 5 from speed)
+
+# print speed results
