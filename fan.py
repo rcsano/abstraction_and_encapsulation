ecee76dd192f2f4a22bebf473ffba9bd2710261c
unknown <rc.sanoiz@gmail.com>
2026-06-03 11:09:36 +0800

    added getter and setter methods for fan properties

--- fan.py
+++ fan.py
@@ -11,6 +11,15 @@ class Fan:
         self.radius = radius
         self.color = color
         self.on = on
-        
+
 # create getter methods to read values
+        def get_speed(self): return self.speed
+        def get_radius(self): return self.radius
+        def get_color(self): return self.color
+        def is_on(self): return self.on
+
 # create setter methods to change values
+        def set_speed(self, speed): self.speed = speed
+        def set_radius(self, radius): self.radius = radius
+        def set_color(self, color): self.color = color
+        def set_on(self, on): self.on = on
