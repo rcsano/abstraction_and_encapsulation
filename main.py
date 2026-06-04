677c7dce41775ebd5488f7b653e4a25ef362225a
unknown <rc.sanoiz@gmail.com>
2026-06-04 18:35:46 +0800

    created flow with 5 times loop for accelerate and decelerate

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # import car class from module file
+from car import Car
 
 # instantiate car object
+def main():
+    my_vehicle = Car("2026", "Porsche 911 GT3 RS")
+    print("Car simulation started.")
 
 # sequence: acceleration (5 iterations)
+    print("Accelerating:")
+    for i in range(1, 6):
+        my_vehicle.accelerate()
+        print("Speed is now:", my_vehicle.get_speed(), "km/h")
 
-# sequence: deceleration (5 iterations)
+# sequence: decelaration (5 iterations)
+    print("Braking:")
+    for i in range(1, 6):
+        my_vehicle.brake()
+        print("Speed is now:", my_vehicle.get_speed(), "km/h")
 
-# display of real-time speed metrics
+if __name__ == "__main__":
+    main()
