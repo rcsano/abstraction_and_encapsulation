825563503776439f50291424ff83df58848b7a25
unknown <rc.sanoiz@gmail.com>
2026-06-04 21:17:53 +0800

    followed the outline and created basic input prompts and display

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 # import pet class
+from pet import Pet
 
 # instantiate pet object and accept keyboard inputs
+def main():
+    my_pet = Pet()
+
+    print("Pet Registry System")
+    name_input = input("Enter pet name: ")
+    type_input = input("Enter animal type: ")
+    age_input = int(input("Enter pet age: "))
+
+    my_pet.set_name(name_input)
+    my_pet.set_animal_type(type_input)
+    my_pet.set_age(age_input)
 
 # display pet data strings
+    print("\nRegistered Data:")
+    print("Name:", my_pet.get_name())
+    print("Type:", my_pet.get_animal_type())
+    print("Age:", my_pet.get_age())
+
+if __name__ == "__main__":
+    main()
